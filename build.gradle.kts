5a46eec3a0bbb04f25198560df23e899ea15b526
Cereal Dev Null <cerealdevnull@gmail.com>
2025-01-07 21:04:03 +0100

    Add proguard rules to keep coroutines
    Add proguard rules to keep coroutines. This is needed to make sure the Cereal application can detect coroutine imports so that it can load its own coroutines version instead of the one bundled with the script to prevent issues.

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,11 @@ allprojects {
         archiveFileName.set("release.jar")
 
         dependencies {
-            // The below dependencies are included in the Cereal client by default. New (breaking) versions will have a different artifact id.
+            // The below dependencies are included in the Cereal client by default so they can be excluded here.
+            // New (breaking) versions will have a different artifact id so they will always stay compatible.
+            // Be careful when adding something here because Proguard could need the code to determine that methods
+            // called by any of these libs are in still in use so that Proguard doesn't remove them. For example
+            // kotlinx-coroutines-core isn't excluded for that reason.
             exclude { dependency ->
                 dependency.moduleGroup == "com.cereal-automation" &&
                     (dependency.moduleName == "cereal-sdk" || dependency.moduleName == "cereal-chrome-driver")
@@ -51,6 +55,7 @@ allprojects {
             files(
                 "${rootDir.absolutePath}/proguard-rules/script.pro",
                 "${rootDir.absolutePath}/proguard-rules/cereal-licensing.pro",
+                "${rootDir.absolutePath}/proguard-rules/coroutines.pro",
             ),
         )
     }
